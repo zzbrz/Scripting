182a26121348a002a6f8a4ea67e8be3c500ea390
zzbrz <119612088+zzbrz@users.noreply.github.com>
2022-12-01 17:09:29 +0100

    Starting Repository
    Mi first script in C# for Unity.

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Imprime por la consola el valor que le damos
+        print("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
