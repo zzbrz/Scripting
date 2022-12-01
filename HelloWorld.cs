d0bbf1019f1be626358b0b1594ed86ddb724696c
JhonRg <rgjonatan@gmail.com>
2022-12-01 17:24:21 +0100

    Release to Alpha

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -7,13 +7,14 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Imprime por la consola el valor que le damos
-        print("Hello, World!");
+        print("coyote");
+        //Imprime al coyote
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print("MIC MIC");
     }
 }
